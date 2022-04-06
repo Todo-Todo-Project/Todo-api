bd6ea941df8ac4ef9bec00561fb6e54c334ae2a2
An <hnan18@vp.fitus.edu.vn>
2022-04-07 00:10:38 +0700

    An | Update app.js

--- app.js
+++ app.js
@@ -7,24 +7,25 @@ const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const todosRouter = require('./Components/todo/index');
-
+const todosRouter = require('./components/todo');
+const authenticationRouter = require('./components/authentication');
 const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cors())
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/todos',todosRouter);
+app.use('/users', authenticationRouter);
+app.use('/todos', todosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
