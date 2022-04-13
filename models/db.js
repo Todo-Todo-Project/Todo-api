75e940c090fc5793210e2f8ab1799645cc002a3f
irisdecembre <thanhtrang181220@gmail.com>
2022-04-13 23:30:40 +0700

    Trang | Fix db

--- models/db.js
+++ models/db.js
@@ -1,4 +1,4 @@
-const { MongoClient, ExplainVerbosity } = require("mongodb");
+const { MongoClient} = require("mongodb");
 // Connection URI
 const uri = process.env.MONGO_URL;
 // Create a new MongoClient
