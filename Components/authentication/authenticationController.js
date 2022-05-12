1f208ae34268ab636dc577414bc3a552e6c00d8f
An <hnan18@vp.fitus.edu.vn>
2022-05-12 12:16:17 +0700

    An | Add google login and register API

--- Components/authentication/authenticationController.js
+++ Components/authentication/authenticationController.js
@@ -3,7 +3,7 @@ const e = require('express');
 const { use } = require('passport');
 const { join } = require('path');
 const authenticationService = require('./authenticationService');
-const {OAuth2Client} = require('google-auth-library');
+const {OAuth2Client, UserRefreshClient} = require('google-auth-library');
 const client = new OAuth2Client(process.env.REACT_APP_GOOGLE_CLIENT_ID);
 
 exports.register = function (req, res) {
@@ -31,8 +31,30 @@ exports.googlelogin = (req, res) => {
   const {tokenId} = req.body;
   client.verifyIdToken({idToken: tokenId, audience: process.env.REACT_APP_GOOGLE_CLIENT_ID}).then(response => {
     const {email_verified, name, email} = response.payload;
+    try {
+      const loginInfo = authenticationService.login(email, email);
+      res.status(200).send({
+        user: { _id: loginInfo._id, email: loginInfo.email },
+        accessToken: authenticationService.createJwt(loginInfo),
+      });
+    } catch (err) {
+      res.status(401).send(err);
+    }
+  })
+};
 
-    console.log(response.payload);
+exports.googleregister = (req, res) => {
+  const {tokenId} = req.body;
+  client.verifyIdToken({idToken: tokenId, audience: process.env.REACT_APP_GOOGLE_CLIENT_ID}).then(response => {
+    const {email_verified, name, email} = response.payload;
+    if (email_verified) {
+      try {
+        const user = authenticationService.register(email, email);
+        res.status(201).send(user);
+      } catch (err) {
+        res.status(500).send(err);
+      }
+    }
   })
 };
 
--- Components/authentication/index.js
+++ Components/authentication/index.js
@@ -5,6 +5,7 @@ const router = express.Router();
 router.post('/register', authenticationController.register);
 router.post('/login', authenticationController.login);
 router.post('/googlelogin', authenticationController.googlelogin);
+router.post('/googleregister', authenticationController.googleregister);
 
 router.get('/', authenticationController.getAllUsers);
 router.get('/:email', authenticationController.getAUser);
